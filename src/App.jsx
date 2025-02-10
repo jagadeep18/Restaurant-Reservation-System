3f7ecac462edeb93756e6da13edcef09e34c5e28
jagadeep18 <gorantla.jagadeep@gmail.com>
2025-02-10 14:25:15 +0000

    commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ReservationSystem from './components/RestaurantReservation'
+
+function App() {
+  return (
+    <>
+      <ReservationSystem/>
+    </>
+  )
+}
+
+export default App
